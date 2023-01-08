aa2193697f24853c29e627e11f5f0a91f51fa50c
Amir Begić <amir.begic2019@size.ba>
2023-01-08 01:01:35 +0100

    category screen implementation

---
+++ lib/views/screens/side_bar_screens/widgets/cetegory_widget.dart
@@ -0,0 +1,51 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class CategoryWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final Stream<QuerySnapshot> _categoriesStream =
+        FirebaseFirestore.instance.collection('Categories').snapshots();
+    return StreamBuilder<QuerySnapshot>(
+      stream: _categoriesStream,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong!');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(
+              color: Colors.black,
+            ),
+          );
+        }
+
+        return GridView.builder(
+          shrinkWrap: true,
+          itemCount: snapshot.data!.size,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 6,
+            mainAxisSpacing: 8,
+            crossAxisSpacing: 8,
+          ),
+          itemBuilder: (context, index) {
+            final categoryData = snapshot.data!.docs[index];
+            return Column(
+              children: [
+                SizedBox(
+                  height: 100,
+                  width: 100,
+                  child: Image.network(categoryData['image']),
+                ),
+                Text(
+                  categoryData['categoryName'],
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+}
